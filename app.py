c7890bbbda57ce26a9a98d2d13178c1a150bfc6f
alisufali <alisufalipetroleum@gmail.com>
2022-09-01 22:12:00 +0430

    Completed app.py v1.0

--- app.py
+++ app.py
@@ -1,40 +1,64 @@
+import pytz
+
 from pathlib import Path
 from typing import Optional
 
 from modules.config import Config
-from modules.menu import Menu
+from modules.database import Database
 from modules.enums import MenuFunctionalities
+from modules.menu import Menu
+from modules.user import UserFunctions
 
 
 class Main:
 
     @classmethod
-    def main(cls, config_path: Optional[Path] = None):
+    def main(cls, config_path: Optional[Path] = None,
+             timezone: pytz.BaseTzInfo = None, indent: int = 2):
 
         Config.load_configs(config_path=config_path)
+        Database.connect_to_database()
 
-        menu = Menu(indent=2)
+        menu = Menu(indent=indent)
 
         while (user_input := int(input(menu))) != \
                 MenuFunctionalities.EXIT.value:
             if user_input == \
                     MenuFunctionalities.ADD_MOVIE.value:
-                pass
+                title = input("Movie title: ")
+                release_date_string = input("Release date (dd-mm-YYYY): ")
+                UserFunctions.add_movie(
+                    title=title, release_date_string=release_date_string,
+                    timezone=timezone
+                )
             elif user_input == \
                     MenuFunctionalities.VIEW_UPCOMMING_MOVIES.value:
-                pass
+                UserFunctions.view_upcomming_movies(
+                    timezone=timezone, indent=2*indent
+                )
             elif user_input == \
                     MenuFunctionalities.VIEW_ALL_MOVIES.value:
-                pass
+                UserFunctions.view_all_movies(
+                    timezone=timezone, indent=2*indent
+                )
             elif user_input == \
                     MenuFunctionalities.WATCH_A_MOVIE.value:
-                pass
+                title = input("Movie title: ")
+                UserFunctions.watch_movie(
+                    title=title
+                )
             elif user_input == \
                     MenuFunctionalities.VIEW_WATCHED_MOVIES.value:
-                pass
+                UserFunctions.view_watched_movies(
+                    timezone=timezone, indent=2*indent
+                )
             else:
                 print("Invalid input please try again.")
 
 
 if __name__ == "__main__":
-    Main.main()
+    indent = 2
+
+    timezone = pytz.timezone("Iran")
+
+    Main.main(timezone=timezone, indent=indent)
